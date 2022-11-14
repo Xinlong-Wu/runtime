140eb6365adb3c51b57e1badfd1cf4f5fb398eb3
WuXinlong <821408745@qq.com>
2022-11-14 16:40:15 +0800

    patch MONO_R_RISCV_JAL

--- src/mono/mono/arch/riscv/riscv-codegen.h
+++ src/mono/mono/arch/riscv/riscv-codegen.h
@@ -614,6 +614,25 @@ enum {
 		                  ((funct5) << 27)); \
 	} while (0)
 
+static G_GNUC_UNUSED inline gboolean
+riscv_is_jal_disp (void *code, void *target)
+{
+	gint64 disp = ((char*)(target) - (char*)(code))/2;
+
+	return (disp > -(1 << 19)) && (disp < (1 << 19));
+}
+
+// static G_GNUC_UNUSED inline unsigned int
+// riscv_get_disp (void *p, void *target)
+// {
+// 	// unsigned int disp = ((char*)target - (char*)p);
+
+// 	// if (target)
+// 	// 	g_assert (riscv_is_jal_disp (p, target));
+
+// 	return 0;
+// }
+
 /*
  * NOTE: When you add new codegen macros or change existing ones, you must
  * expand riscv-codegen-test.c to cover them, and update riscv-codegen.exp32
