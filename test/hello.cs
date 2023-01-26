dd8633f7d0e15ea7d768d58b765e2dbf3ed4ab69
WuXinlong <821408745@qq.com>
2023-01-26 21:08:27 +0800

    change testcase

--- test/hello.cs
+++ test/hello.cs
@@ -5,12 +5,17 @@ namespace HelloWorld
   class Program
   {
     static int Main(string[] args)
-    {
-      return 1;
+  	{
+    //   Console.WriteLine("Hello");
+      return test(args);
     }
-
-    static int test(int i){
-      return i;
-    }
+		
+		static int test(string[] args){
+			Random rd = new Random();
+			if(args.Length > 0)
+				return rd.Next(0,10);
+			else
+				return 0;
+		}
   }
 }
