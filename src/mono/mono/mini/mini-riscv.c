8fb81e4b643d08e1e57af5d71d30aa8ca100cdd7
WuXinlong <821408745@qq.com>
2023-02-16 11:47:41 +0800

    lowering OP_SUB_IMM

--- src/mono/mono/mini/mini-riscv.c
+++ src/mono/mono/mini/mini-riscv.c
@@ -1259,6 +1259,7 @@ mono_arch_decompose_opts (MonoCompile *cfg, MonoInst *ins)
 		case OP_LADD_IMM:
 		case OP_IADD_IMM:
 		case OP_LAND_IMM:
+		case OP_ISUB_IMM:
 		case OP_LOR:
 		case OP_ICONV_TO_I:
 		case OP_ICONV_TO_U:
@@ -1634,6 +1635,9 @@ loop_start:
 					ins->inst_imm = 0;
 				}
 				break;
+			case OP_SUB_IMM:
+				ins->inst_imm = -ins->inst_imm;
+				ins->opcode = OP_ADD_IMM;
 			// Inst ADDI use I-type Imm
 			case OP_ADD_IMM:
 			case OP_IADD_IMM:
