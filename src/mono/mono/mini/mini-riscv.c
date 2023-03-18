51a203449d1dca895dd479c71a6099cda2287f8a
WuXinlong <821408745@qq.com>
2023-03-18 21:56:57 +0800

    OP_LCONV_TO_OVF_U

--- src/mono/mono/mini/mini-riscv.c
+++ src/mono/mono/mini/mini-riscv.c
@@ -1729,6 +1729,7 @@ mono_arch_decompose_opts (MonoCompile *cfg, MonoInst *ins)
 		case OP_LREM_UN:
 
 		case OP_ICONV_TO_OVF_U2:
+		case OP_LCONV_TO_OVF_U:
 		case OP_LCONV_TO_OVF_I4_UN:
 		case OP_LCONV_TO_OVF_U4_UN:
 
